38a81c5add228df809aeb3bc238516d910a694b0
Egor Leskov <egor.leskov.03@mail.ru>
2021-12-24 11:10:09 +0300

    add connect to database add bottomNavigation

--- app/src/main/java/com/example/electronicjournal/MainActivity.java
+++ app/src/main/java/com/example/electronicjournal/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.electronicjournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.os.TokenWatcher;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,48 +31,66 @@ import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Переменные для Sql
-    private static String ip = "192.168.206.17";
-    private static String port = "1433";
-    private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
-    private static String database = "ElectronikJournalDB";
-    private static String username = "admin";
-    private static String password = "admin";
-    private static String Url = "jdbc:jtds:sqlserver://"+port+"/"+database+";instance=SQLEXPRESS";
-    //подключение
-    private Connection connection = null;
-    //потоки
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
+    //FireBase
+    private FirebaseAuth mAuth;
     //кнопки
-
+    EditText Login, Password;
+    Button loginbtn;
+    //навигация
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        try {
-            Class.forName(Classes);
-            connection = DriverManager.getConnection(Url, username, password);
-            Log.i("Connect", "Успешно");
-        } catch (SQLException | ClassNotFoundException throwables) {
-            Log.i("Connect", "Неудачно");
-            throwables.printStackTrace();
-        }
-    }
-    private void SqlConnectBt(View v){
-        TextView login = findViewById(R.id.login);
-        TextView pass = findViewById(R.id.pass);
+        mAuth = FirebaseAuth.getInstance();
 
-        if (login.getText() == null || pass.getText() == null){
-            Toast.makeText(this,"Поля не должны быть пустыми",Toast.LENGTH_LONG);
+        Login = findViewById(R.id.login);
+        Password = findViewById(R.id.pass);
+        loginbtn = findViewById(R.id.BtSql);
+
+        loginbtn.setOnClickListener(view -> {
+            loginUser();
+        });
+
+
+    }
+
+    private void loginUser() {
+        String login = Login.getText().toString();
+        String password = Password.getText().toString();
+
+        if (TextUtils.isEmpty(login)){
+            Login.setError("Логин не должен быть пустым");
+            Login.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            Password.setError("Пароль не должен быть пустым");
+            Password.requestFocus();
         }
         else {
-            String loginDB = login.getText().toString();
-            String passDB = pass.getText().toString();
+            mAuth.signInWithEmailAndPassword(login,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(MainActivity.this,"Вход выполнен",Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,"Ошибка входа" + task.getException(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         }
     }
 }
---
+++ app/src/main/java/com/example/electronicjournal/ProfileActivity.java
@@ -0,0 +1,40 @@
+package com.example.electronicjournal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.Table:
+                                startActivity(new Intent(ProfileActivity.this,TableActivity.class));
+                                finish();
+                                break;
+                            case R.id.Settings:
+                                startActivity(new Intent(ProfileActivity.this,SettingsActivity.class));
+                                finish();
+                                break;
+                        }
+                        return false;
+                    }
+                }
+        );
+    }
+}
